851411616ddd38b52ee27870b6d430a0f33becc8
허재영 <130130273+tommyheo@users.noreply.github.com>
2023-04-12 16:05:52 +0900

    Day01

---
+++ Hello.java
@@ -0,0 +1,25 @@
+package day01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// package는 항상 소문자로 제목 정하기
+		// class 제목은 무조건 대문자로 시작해주기
+		// 모든 프로그램의 시작은 항상 public static void main으로 시작해야한다
+		// 여기까지가 항상 기본 틀임
+		// } 더블클릭하면 {}의 모든 것을 선택해줌
+		
+		System.out.println("Hellow world!");	// Hellow world!
+		// System.out.print(""); : 괄호안에 있는 문자를 출력해달라는 명령
+		// 여러개 출력시 같은 라인에 출력이 된다
+		// System.out.println(""); : 괄호안에 있는 문자를 출력하고 다음 라인으로 넘어가 달라는 명령
+		//  ln == line : 다음 출력을 다음 라인에 해달라는 명령 html에서의 br태그와 같다고 생각하면 됨
+		// 에러 시 !나 빨간색으로 표시해줌
+		
+		System.out.println("허재영");		// 허재영
+		// 이 파일의 컴파일러 class파일은 해당 폴더 상위 폴더인 bin폴더에 들어있다
+		// Outline은 소스코드를 파악할 수 있다
+	}
+
+}
